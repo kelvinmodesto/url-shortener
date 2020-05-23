15679791391f2fae0947be7772f2fd5afc9370df
Kelvin Modesto <km.modesto@gmail.com>
2020-05-23 12:39:45 -0300

    create db files

---
+++ src/db/base/index.ts
@@ -0,0 +1,8 @@
+import ContextStrategy from "./contextStrategy";
+
+const main = {
+  ContextStrategy
+};
+
+export { main, ContextStrategy };
+export default main;
---
+++ src/db/mongodb/index.ts
@@ -0,0 +1,8 @@
+import MongoDBStrategy from "./mongoDBStrategy";
+
+const main = {
+  MongoDBStrategy
+};
+
+export { main, MongoDBStrategy };
+export default main;
